7e5c2f2ed1a6c36c19fc4fc91f7178e925f0c0cc
chajus1 <chajus1@googlemail.com>
2023-07-04 21:00:01 +0100

    IComparable<Duck> Interface

--- Ducks8/Duck.cs
+++ Ducks8/Duck.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace Ducks8
 {
-    class Duck
+    class Duck : IComparable<Duck>
     {
         public int Size { get; set; }
         public KindOfDuck Kind { get; set; }
+
+        public int CompareTo(Duck duckToCompare)
+        {
+            if (Size > duckToCompare.Size)
+                return 1;
+            else if (Size < duckToCompare.Size)
+                return -1;
+            else
+                return 0;
+        }
 
     }
 }
